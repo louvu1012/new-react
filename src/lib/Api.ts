f8ee21e84d643f171d3900aa727fbc9619b8db37
Luong <luongvh@nextgsolutions.com>
2024-05-24 17:24:43 +0700

    test

--- src/lib/Api.ts
+++ src/lib/Api.ts
@@ -2,6 +2,7 @@ import axios from "axios"
 
 export const Api = axios.create({
   // timeout: 20000,
-  baseURL: 'https://nodejs-auth-pi.vercel.app', // Địa chỉ cơ sở của API
+  baseURL: 'https://nodejs-auth-pi.vercel.app/api/auth/', // Địa chỉ cơ sở của API
+  // baseURL: 'http://localhost:5000/api/auth/',
   withCredentials: true, // Cho phép gửi cookie
 })
